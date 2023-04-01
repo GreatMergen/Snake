563f6a8472d16f9291685048114f512159c20330
Emre Yıldırım <mergenthegreat@gmail.com>
2023-04-01 15:53:50 +0300

    denmee

--- Assets/_Scripts/Managers/LevelManager.cs
+++ Assets/_Scripts/Managers/LevelManager.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 public class LevelManager : MonoBehaviour
@@ -5,4 +6,5 @@ public class LevelManager : MonoBehaviour
    //dasldşajsdşasdkasşdas
    //daslşdasadşi
    //ddkdkd
+   
 }
